26c78f0bc065294f9e0fa90fada0a1207456a110
mzybura95 <mzybura95@gmail.com>
2023-03-20 16:39:13 +0000

    login_mode_changes

--- login_mode.sh
+++ login_mode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Select app:
 read appname
-grep defaultloginmode /opt/apache/tomcat/latest/webapps/$appname/WEB-INF/xml/sys_defaults.properties.xml
+grep defaultloginmode /opt/apache/tomcat/latest/webapps/$appname/WEB-INF/xml/sys_defaults.properties
